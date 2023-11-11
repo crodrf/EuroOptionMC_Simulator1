1734a1ad489484d7be22a6847a37a645fa6c35aa
Carlos Rodriguez Fernandez <crodrf04@gmail.com>
2023-11-11 00:48:07 -0800

    update

--- EuroOptionMC_StaticLib/Payoffs.cpp
+++ EuroOptionMC_StaticLib/Payoffs.cpp
@@ -53,3 +53,17 @@ namespace payoff
 		return std::max(0.0, strike_price_ - spot_price);
 	}
 }
+
+
+/*
+ * This portion of code is adapted from Mark Joshi's work in "C++ Design Patterns and Derivatives Pricing".
+ * Copyright (c) 2002 Mark Joshi
+ *
+ * Permission to use, copy, modify, distribute and sell this
+ * software for any purpose is hereby granted without fee, provided that the above copyright notice
+ * appear in all copies and that both that copyright notice and
+ * this permission notice appear in supporting documentation.
+ * Mark Joshi makes no representations about the
+ * suitability of this software for any purpose. It is provided
+ * "as is" without express or implied warranty.
+ */
--- EuroOptionMC_StaticLib/StatisticsHandler.cpp
+++ EuroOptionMC_StaticLib/StatisticsHandler.cpp
@@ -1,5 +1,5 @@
-#include "pch.h" 
-#include "StatisticsHandler.h" 
+#include "pch.h"
+#include "StatisticsHandler.h"
 
 namespace stochastic_methods
 {
@@ -66,3 +66,17 @@ namespace stochastic_methods
 		return stats_object_ ? stats_object_->clone() : nullptr;
 	}
 }
+
+
+/*
+ * This portion of code is adapted from Mark Joshi's work in "C++ Design Patterns and Derivatives Pricing".
+ * Copyright (c) 2002 Mark Joshi
+ *
+ * Permission to use, copy, modify, distribute and sell this
+ * software for any purpose is hereby granted without fee, provided that the above copyright notice
+ * appear in all copies and that both that copyright notice and
+ * this permission notice appear in supporting documentation.
+ * Mark Joshi makes no representations about the
+ * suitability of this software for any purpose. It is provided
+ * "as is" without express or implied warranty.
+ */
